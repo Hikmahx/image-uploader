7fff638ce58c01d2884acba176de7c9abf1cc077
Hikmahx <hikmayousuph@gmail.com>
2022-04-26 14:23:52 -1100

    tailwind config file updated

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,13 +5,22 @@ module.exports = {
       boxShadow: {
         xl: "0px 4px 12px rgba(0, 0, 0, 0.1)",
       },
+      minHeight: {
+        xl: "29.5rem",
+      },
+      maxWidth: {
+        xl: "25.1rem",
+      },
+      borderWidth: {
+        1: "1px",
+      },
     },
     colors: {
       blue: "#2f80ed",
       white: "#ffffff",
       gray: "#828282",
       green: "#219653",
-      "very-light-gray": "#fafafb",
+      "light-grayish-blue": "#fafafb",
       "light-gray": "#bdbdbd",
       "dark-gray": "#4f4f4f",
       "grayish-blue": "#f6f8fb",
@@ -20,7 +29,7 @@ module.exports = {
       "gray-progress-bar": "#f2f2f2",
     },
     fontFamily: {
-      poppins: ["Poppins, sans-serif"]
+      poppins: ["Poppins, sans-serif"],
     },
   },
   plugins: [],
