9e9fb357ca2999079e6ced196c5392a04af9efd7
Hikmahx <hikmayousuph@gmail.com>
2022-04-28 22:37:38 -1100

    server updated, mutler replaced express-fileupload

--- server.js
+++ server.js
@@ -1,34 +1,44 @@
 const express = require("express");
-const fileUpload = require("express-fileupload");
+const multer = require("multer");
+// const upload = multer({ dest: __dirname + "/client/public/uploads/" });
+// const upload = multer();
 
 const app = express();
 
-// File Upload
-app.use(fileUpload())
+const imageStorage = multer.diskStorage({
+  
+  destination: (req, file, cb) => {
+    cb(null, `${__dirname}/client/public/uploads`);
+  },
+  filename: (req, file, cb) => {
+
+    // prefix filename with date
+    cb(null, Date.now() + "---" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: imageStorage });
 
 // Upload Endpoint
-app.post('/upload', (req, res)=>{
-  if(req.files ===null){
-
+app.post('/upload', upload.single("file"), async (req, res) => {
+  if (req.file === null) {
     // Bad Code
-    return res.status(400).json({msg: 'No file uploaded'})
+    return res.status(400).json({ msg: "No file uploaded" });
   }
 
-  // .file is gotten from react 
-  const file = req.files.file
-
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err=>{
-    if(err){
-      console.log(err)
-      // Server error
-      return res.status(500).send(err)
-    }
-
-    res.status(200).json({fileName: file.name, filePath: `/uploads/${file.name}`})
-  })
-})
+  // .file is gotten from react (input name="file")
+  const file = req.file;
+  console.log(file);
+  
+    return res
+      .status(200)
+      .json({ fileName: file.filename, filePath: `/uploads/${file.filename}` });
 
 
-const PORT = 5000; 
+});
 
-app.listen(PORT, ()=>{console.log('This is running on PORT: ' + PORT)})
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log("This is running on PORT: " + PORT);
+});
