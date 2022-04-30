36009a794363fd4c5f846af1abee93af5f84f670
Hikmahx <hikmayousuph@gmail.com>
2022-04-29 18:18:35 -1100

    dropzone implemented

--- client/src/components/FileUpload.jsx
+++ client/src/components/FileUpload.jsx
@@ -1,12 +1,11 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import imageUpload from "../assets/image-upload.svg";
 import UploadContext from "../context/UploadContext";
+import Dropzone from "react-dropzone";
 
 const FileUpload = () => {
+  const { filename, uploadedFile, onChange } = useContext(UploadContext);
 
-  const {filename, uploadedFile, onChange} = useContext(UploadContext)
-
-  
   return (
     <div className="bg-white w-full max-w-xl px-8 py-9 m-4 min-h-xl flex flex-col items-center rounded-xl shadow-xl">
       <div className="w-full">
@@ -15,12 +14,25 @@ const FileUpload = () => {
           <h2 className="text-xs text-gray mb-6">
             File should be Jpeg, Png,...
           </h2>
-          <div className="w-full h-56 p-6 rounded-lg bg-grayish-blue border-2 border-dashed border-pale-blue-border flex flex-col items-center">
-            <div className="img-wrapper w-2/5 mb-5">
-              <img src={imageUpload} alt="upload" className="w-full" />
-            </div>
-            <div className="text-light-gray text-xs">{filename}</div>
-          </div>
+          <Dropzone
+            multiple={false}
+            onDrop={(acceptedFiles) => console.log(acceptedFiles[0])}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <div
+                {...getRootProps({
+                  className:
+                    "w-full h-56 p-6 rounded-lg bg-grayish-blue border-2 border-dashed border-pale-blue-border flex flex-col items-center realtive",
+                })}
+              >
+                <input {...getInputProps()} />
+                <div className="img-wrapper w-2/5 mb-5">
+                  <img src={imageUpload} alt="upload" className="w-full" />
+                </div>
+                <div className="text-light-gray text-xs">{filename}</div>
+              </div>
+            )}
+          </Dropzone>
           <div className="text-light-gray m-6 text-xs">Or</div>
           <div className="bg-blue text-white px-4 h-8 relative rounded-md text-xs flex items-center justify-center cursor-pointer hover:border-blue hover:border-1 hover:bg-white hover:text-blue transition-all">
             <p className="m-auto">Choose a file</p>
